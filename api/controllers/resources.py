68e2e0afd125af9a941855bbdac6805d5bb03ee5
nmasscodes <nclbnoah@gmail.com>
2024-11-06 18:33:35 -0500

    Initial commit, added all routes and controllers.

---
+++ api/controllers/resources.py
@@ -0,0 +1,43 @@
+from sqlalchemy.orm import Session
+from fastapi import HTTPException, status, Response, Depends
+from ..models import models, schemas
+
+
+def create(db: Session, resource: schemas.ResourceCreate):
+    db_resource = models.Resource(
+        item=resource.item,
+        amount=resource.amount
+    )
+    db.add(db_resource)
+    db.commit()
+    db.refresh(db_resource)
+    return db_resource
+
+
+def read_all(db: Session):
+    return db.query(models.Resource).all()
+
+
+def read_one(db: Session, resource_id: int):
+    return db.query(models.Resource).filter(models.Resource.id == resource_id).first()
+
+
+def update(db: Session, resource_id: int, resource: schemas.ResourceUpdate):
+    db_resource = db.query(models.Resource).filter(models.Resource.id == resource_id)
+    if not db_resource.first():
+        raise HTTPException(status_code=404, detail="Resource not found")
+
+    update_data = resource.dict(exclude_unset=True)
+    db_resource.update(update_data, synchronize_session=False)
+    db.commit()
+    return db_resource.first()
+
+
+def delete(db: Session, resource_id: int):
+    db_resource = db.query(models.Resource).filter(models.Resource.id == resource_id)
+    if not db_resource.first():
+        raise HTTPException(status_code=404, detail="Resource not found")
+
+    db_resource.delete(synchronize_session=False)
+    db.commit()
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
---
+++ api/controllers/sandwiches.py
@@ -0,0 +1,40 @@
+from sqlalchemy.orm import Session
+from fastapi import HTTPException, status, Response, Depends
+from ..models import models, schemas
+
+
+def create(db: Session, sandwich: schemas.SandwichCreate):
+    db_sandwich = models.Sandwich(
+        sandwich_name=sandwich.sandwich_name,
+        price=sandwich.price
+    )
+    db.add(db_sandwich)
+    db.commit()
+    db.refresh(db_sandwich)
+    return db_sandwich
+
+
+def read_all(db: Session):
+    return db.query(models.Sandwich).all()
+
+
+def read_one(db: Session, sandwich_id: int):
+    return db.query(models.Sandwich).filter(models.Sandwich.id == sandwich_id).first()
+
+
+def update(db: Session, sandwich_id: int, sandwich: schemas.SandwichUpdate):
+    db_sandwich = db.query(models.Sandwich).filter(models.Sandwich.id == sandwich_id)
+    if not db_sandwich.first():
+        raise HTTPException(status_code=404, detail="Sandwich not found")
+
+    update_data = sandwich.dict(exclude_unset=True)
+    db_sandwich.update(update_data, synchronize_session=False)
+    db.commit()
+    return db_sandwich.first()
+
+
+
+
+
+
+
